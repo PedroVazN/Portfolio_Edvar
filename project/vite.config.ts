e129a25b9357781e1f6af33cb041aa6a5164fe22
PedroVazN <vaznascimento23@gmail.com>
2025-01-26 12:44:51 -0300

    Update vite.config

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  base: './',  // Ajusta caminhos relativos para funcionar corretamente na Vercel
+  build: {
+    outDir: 'dist',  // Garante que a Vercel reconheça a pasta de saída correta
   },
 });
